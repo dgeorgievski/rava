065a46fef8187a7a7b72b0e725abb91b5bb119ec
Dimitar Georgievski <dgeorgievski@gmail.com>
2023-11-21 22:14:15 +0000

    testing actix-web routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, 
+    get, 
+    middleware, 
+    App, 
+    HttpRequest, 
+    HttpResponse, 
+    HttpServer, 
+    Responder};
+
+#[get("/")]
+async fn greet() -> HttpResponse  {
+    HttpResponse::Ok().body("Hello, World!")
+}
+
+#[get("/{name}")]
+async fn greet_name(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+        web::scope("/hello")
+                    .service(greet)
+                    .service(greet_name),
+            )
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
