1ebba6b0469d45411f74a6176b9da4334232a77f
Dimitar Georgievski <dgeorgievski@gmail.com>
2023-11-30 17:08:59 -0500

    added healthz handler

---
+++ doc/setup.md
@@ -0,0 +1,15 @@
+# Setup of dev environment
+
+Using Luca Palmieri's suggestions for configuring inner dev loop in Rust.
+https://www.lpalmieri.com/posts/2020-06-06-zero-to-production-1-setup-toolchain-ides-ci/
+
+## Cargo tools
+
+```sh
+cargo install cargo-watch \
+ cargo-tarpaulin \
+ cargo-audit \
+ cargo-expand \
+ cargo-edit
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,6 @@
-use actix_web::{web, 
-    get, 
-    middleware, 
-    App, 
-    HttpRequest, 
-    HttpResponse, 
-    HttpServer, 
-    Responder};
-
-#[get("/")]
-async fn greet() -> HttpResponse  {
-    HttpResponse::Ok().body("Hello, World!")
-}
-
-#[get("/{name}")]
-async fn greet_name(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use rava::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(
-        web::scope("/hello")
-                    .service(greet)
-                    .service(greet_name),
-            )
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
